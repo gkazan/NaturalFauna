b078e4f7e55d63fde64241e09ac862677379f005
ikerleon02 <ikerleon02@users.noreply.github.com>
2018-05-02 22:02:35 +0200

    puffin animation try

--- src/main/java/com/ikerleon/naturalfaunamod/client/model/ModelPuffin.java
+++ src/main/java/com/ikerleon/naturalfaunamod/client/model/ModelPuffin.java
@@ -3,6 +3,7 @@ package com.ikerleon.naturalfaunamod.client.model;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 
 public class ModelPuffin extends ModelBase {
     public ModelRenderer leftleg;
@@ -79,11 +80,20 @@ public class ModelPuffin extends ModelBase {
         this.leftleg.render(f5);
         this.head.render(f5);
         this.rightleg.render(f5);
+        
     }
     
     public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
         modelRenderer.rotateAngleZ = z;
+       
+    }
+    
+    @Override
+    public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity par7Entity) 
+    {
+    	this.rightleg.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.4F * par2;
+    	this.leftleg.rotateAngleX = MathHelper.cos(par1 * 0.6662F + (float)Math.PI) * 1.4F * par2;
     }
 }
