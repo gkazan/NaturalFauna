4757cd6576119db33f2f253735c8d9942dc56d90
George Kazanjian <soggymustacheofficialmods@gmail.com>
2018-05-03 15:40:56 -0400

    Work on puffin

--- src/main/java/com/ikerleon/naturalfaunamod/client/render/RenderPuffin.java
+++ src/main/java/com/ikerleon/naturalfaunamod/client/render/RenderPuffin.java
@@ -1,6 +1,7 @@
 package com.ikerleon.naturalfaunamod.client.render;
 
 import org.zawamod.client.render.entity.base.RenderLivingZAWA;
+import org.zawamod.util.ZAWARenderUtils;
 
 import com.ikerleon.naturalfaunamod.NFReference;
 import com.ikerleon.naturalfaunamod.client.model.ModelPuffin;
@@ -8,6 +9,7 @@ import com.ikerleon.naturalfaunamod.entity.EntityPuffin;
 
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.relauncher.Side;
@@ -25,11 +27,11 @@ public class RenderPuffin extends RenderLivingZAWA<EntityPuffin> {
 	
     protected ResourceLocation getEntityTexture(EntityPuffin entity)
     {
-    	if(entity.isChild()) {
-    		return getTextureOfVarChild(entity.getAnimalType());
+    	if(entity.isChild() || entity.isInWater()) {
+    		return ZAWARenderUtils.none;
     	}
     	else {
-		return getTextureOfVar(entity.getAnimalType());
+    		return getTextureOfVar(entity.getAnimalType());
     	}
     }
 
@@ -47,6 +49,49 @@ public class RenderPuffin extends RenderLivingZAWA<EntityPuffin> {
 			case 0:
 			default:
 				return texturechild;
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public class LayerHippo implements LayerRenderer<EntityPuffin> {
+	    private final RenderPuffin render;
+	    private final ModelPuffin modelBaby = new ModelPuffin();
+	    private final ModelPuffin modelBabyWater = new ModelPuffin();
+	    private final ModelPuffin modelWater = new ModelPuffin();
+		 
+	    public LayerHippo(RenderPuffin re)
+	    {
+	        this.render = re;
+	    }
+	 
+	    public void doRenderLayer(EntityPuffin kat, float f, float f1, float f2, float f3, float f4, float f5, float f6)
+	    {	    	
+	        if (!kat.isInvisible())
+	        {
+	        	if(kat.isChild() && kat.isInWater()) {
+		            this.render.bindTexture(this.render.getTextureOfVar(kat.getAnimalType()));
+		            this.modelBabyWater.setModelAttributes(this.render.getMainModel());
+		            this.modelBabyWater.setRotationAngles(f,f1,f3,f4,f5,f6, kat);
+		            this.modelBabyWater.render(kat, f, f1, f2, f3, f4, f6);
+	        	}
+	        	else if(kat.isChild()) {
+		            this.render.bindTexture(this.render.getTextureOfVar(kat.getAnimalType()));
+		            this.modelBaby.setModelAttributes(this.render.getMainModel());
+		            this.modelBaby.setRotationAngles(f,f1,f3,f4,f5,f6, kat);
+		            this.modelBaby.render(kat, f, f1, f2, f3, f4, f6);
+	        	}
+	        	else if(kat.isInWater()) {
+	        		this.render.bindTexture(this.render.getTextureOfVar(kat.getAnimalType()));
+		            this.modelWater.setModelAttributes(this.render.getMainModel());
+		            this.modelWater.setRotationAngles(f,f1,f3,f4,f5,f6, kat);
+		            this.modelWater.render(kat, f, f1, f2, f3, f4, f6);
+	        	}
+	        }
+	    }
+	    
+		@Override
+		public boolean shouldCombineTextures() {
+			return true;
 		}
 	}
 	
